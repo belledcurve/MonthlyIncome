fc1ebeb2a811dcd9743a399034d75447944644d3
SAMUEL LEE <samuellee@nyu.edu>
2022-06-25 11:57:45 +0900

    Update monthlyincome.R
    Added function to set working hours.

--- monthlyincome.R
+++ monthlyincome.R
@@ -25,7 +25,7 @@ AddWork <- function(time, work, plus) {
       break
     }
   }
-
+  
   return.value <- function.income %>%
     mutate("total" = case_when (category == ".hagwon" ~ (hours * 17000)
                                 ,category == ".co" ~ (hours * 30000)
@@ -37,6 +37,30 @@ AddWork <- function(time, work, plus) {
   income.raw <<- return.value    
   return(return.value)
 }
+
+
+SetWork <- function(time, work, set) {
+  
+  function.income <- income.raw
+  for (a in 1:length(function.income$month)) {
+    if (identical(time, function.income$month[a]) & identical(work, function.income$category[a])) {
+      function.income$hours[a] <- set
+      break
+    }
+  }
+  
+  return.value <- function.income %>%
+    mutate("total" = case_when (category == ".hagwon" ~ (hours * 17000)
+                                ,category == ".co" ~ (hours * 30000)
+                                ,category == ".tutoring" ~ (hours * 30000 * (1/0.967)))) %>%
+    mutate("before.tax" = total, "after.tax" = total * 0.967) %>%
+    group_by(month) %>% 
+    mutate("monthly.total" = sum(after.tax, na.rm = TRUE))
+  
+  income.raw <<- return.value    
+  return(income.raw)
+}
+
 
 reset <- function() {
   return(
@@ -53,6 +77,9 @@ SaveIncome <- function() {
 AddMonth <- function(month, work) {
   new <- c(month, work, 0)
   income.raw <<- rbind(income.raw, new)
-  return(income.raw)
+  return(
+    income.raw
+  )
 }
+
 
